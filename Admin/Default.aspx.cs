b5e8ea6c411e5308043786e3d08f09227bb3f9df
Chris Wood <chriswood.ca@gmail.com>
2011-05-04 14:59:57 -0700

    Documentation for code added for Admin/Default.aspx

--- Admin/Default.aspx.cs
+++ Admin/Default.aspx.cs
@@ -19,10 +19,13 @@ public partial class Admin_Default : System.Web.UI.Page
     }
 
     /// <summary>
-    ///
+    /// Postback event selecting the mode of the system user administration
+    /// section of the admin page. Two modes:
+    /// Create - Shows the UI for creating a new user
+    /// Edit - Shows the UI for editing a existing user
     /// </summary>
-    /// <param name="sender"></param>
-    /// <param name="e"></param>
+    /// <param name="sender">not used in our code</param>
+    /// <param name="e">not used in our code</param>
     protected void rblUsers_SelectedIndexChanged(object sender, EventArgs e)
     {
         switch(rblUsers.SelectedValue) {
@@ -40,10 +43,14 @@ public partial class Admin_Default : System.Web.UI.Page
     }
     
     /// <summary>
-    /// 
+    /// Postback event for selecting information for a user's role in the
+    /// system user administration section of the admin page. Two Roles:
+    /// Safety Officer - hides department dropdownmenu
+    /// Lab Manager - shows department dropdownmenu
+    /// Department shows what lab the Lab Manager looks after.
     /// </summary>
-    /// <param name="sender"></param>
-    /// <param name="e"></param>
+    /// <param name="sender">not used in our code</param>
+    /// <param name="e">not used in our code</param>
     protected void rblUserRole_SelectedIndexChanged(object sender, EventArgs e)
     {
         switch (rblUserRole.SelectedValue)
