cd1862eb1c698dfa7d602402a145c8fa1cc8bebc
Chris Wood <chriswood.ca@gmail.com>
2011-05-14 13:53:58 -0700

    UI for veiewing of reports that need follow up.
    Lab Follow up viewing of submitting reports, no follow up yet.

--- Followup/Default.aspx.cs
+++ Followup/Default.aspx.cs
@@ -14,10 +14,15 @@ public partial class Followup_Default : System.Web.UI.Page
 
     //Database Entities
     BCCAEntities ctx = new BCCAEntities();
+    //Used to pass report or inspection number to Followup.aspx
+    public string followupNo { get; set; }
+    //Used to determine if report or inspection on Followup.aspx
+    public string followupType { get; set; }
 
     protected void Page_Load(object sender, EventArgs e)
     {
         Populate_Incidents();
+        Populate_Labs();
     }
 
     #region Incident/Accident Followup
@@ -28,25 +33,79 @@ public partial class Followup_Default : System.Web.UI.Page
     /// </summary>
     protected void Populate_Incidents()
     {
-        //var q = ctx.Incidents
-        //    .Where(inc => (inc.followUpStatus == "0") || (inc.followUpStatus == "1")
-        //        //hardcoded "person logged in department"
-        //    && (inc.deptNo == "3"))
-        //    .Select(inc => new 
-        //    { 
-        //        incNo = inc.incidentNo,
-        //        incDate = inc.p1_dateOfIncident,
-        //        incEmpName = (inc.Employee.fname + " ") + inc.Employee.lname
-        //    });
-        //q.Count();
-        //gvwFollowupIncidents.DataSource = q;
-        //gvwFollowupIncidents.DataBind();
+        gvwFollowupIncidents.DataSource = ctx.Incidents
+            .Where(inc => ((inc.followUpStatus == "0") || (inc.followUpStatus == "1"))
+                //hardcoded "person logged in department"
+            && (inc.submitterDeptNo == 3))
+            .Select(inc => new
+            {
+                incNo = inc.incidentNo,
+                incDate = inc.p1_dateOfIncident,
+                incEmpName = (inc.Employee.fname + " ") + inc.Employee.lname,
+                incSubmitter = inc.reportSubmitter
+            })
+            .OrderBy(a => a.incDate);
+        gvwFollowupIncidents.DataBind();
+    }
+
+    /// <summary>
+    /// TODO:
+    /// </summary>
+    /// <param name="sender">not used in our code</param>
+    /// <param name="e">not used in our code</param>
+    protected void gvwFollowupIncidents_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        followupNo = gvwFollowupIncidents.SelectedRow.Cells[1].Text;
+        followupType = "Incident";
+        Server.Transfer("Followup.aspx", true);
     }
     #endregion
 
     #region Lab Inspection Followup
+    /// <summary>
+    /// TODO:
+    /// </summary>
+    protected void Populate_Labs()
+    {
+        gvwFollowupLabInspection.DataSource = ctx.LabInspections
+            .Where(l => ((l.followUpStatus == "0") || (l.followUpStatus == "1"))
+                //hardcoded "person logged in department"
+            && (l.deptNo == 3))
+            .Select(l => new
+            {
+                insNo = l.labInsNo,
+                insDate = l.date,
+                insLoc = (ctx.Departments
+                    .Where(d => d.deptNo == l.deptNo)
+                    .Select(d => d.deptName)
+                    .FirstOrDefault() + " - ") + l.room,
+                insMgr = l.labMgr,
+                insSubmitter = l.reportSubmitter
+            })
+            .OrderBy(a => a.insDate);
+        gvwFollowupLabInspection.DataBind();
+    }
+
+    /// <summary>
+    /// TODO:
+    /// </summary>
+    /// <param name="sender">not used in our code</param>
+    /// <param name="e">not used in our code</param>
+    protected void gvwFollowupLabInspection_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        followupNo = gvwFollowupLabInspection.SelectedRow.Cells[1].Text;
+        followupType = "Lab";
+        Server.Transfer("Followup.aspx", true);
+    }
     #endregion
 
     #region Office Inspection Followup
+    /// <summary>
+    /// TODO:
+    /// </summary>
+    protected void Populate_Offices()
+    { }
     #endregion
+
+    
 }
---
+++ Followup/Followup.aspx.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Linq;
+using BCCAModel;
+using System.Web.UI.WebControls;
+
+/// <summary>
+///Followup/Followup.aspx.cs
+///BCCA Cancer Research Centre
+///Safety Training Database and Website
+///Author: BCIT COMP4900 2011
+///Chris Wood - chriswood.ca@gmail.com
+/// </summary>
+public partial class Followup_Followup : System.Web.UI.Page
+{
+    //Database Entities
+    BCCAEntities ctx = new BCCAEntities();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        try
+        {
+            switch (PreviousPage.followupType)
+            {
+                case "Incident":
+                    break;
+                case "Lab":
+                    Populate_Lab_Followup(Convert.ToInt32(PreviousPage.followupNo));
+                    break;
+                case "Office":
+                    break;
+                default:
+                    throw new System.SystemException("Default case of switch should never be reached");
+            }
+        }
+        //If execption thrown, no data from previous page; Should not be here unless transfered
+        //from Default.aspx in this same directory with selected report information
+        catch (Exception ex)
+        {
+            Response.Redirect("Default.aspx");
+        }
+    }
+
+    #region Incident Followup
+
+    #endregion
+
+    #region Lab Followup
+    protected void Populate_Lab_Followup(int insNo)
+    {
+        gvwLabFollowup.DataSource = ctx.LabInspections
+                                  .Join(
+                                     ctx.LabInspectionDetails,
+                                     LI => LI.labInsNo,
+                                     LID => LID.labInsNo,
+                                     (LI, LID) =>
+                                        new
+                                        {
+                                            LI = LI,
+                                            LID = LID
+                                        }
+                                  )
+                                  .Join(
+                                     ctx.LabInspectionItems,
+                                     temp0 => temp0.LID.labItemNo,
+                                     LII => (Int32?)(LII.labInsItemNo),
+                                     (temp0, LII) =>
+                                        new
+                                        {
+                                            temp0 = temp0,
+                                            LII = LII
+                                        }
+                                  )
+                                  .Where(temp1 => (temp1.temp0.LI.labInsNo == insNo))
+                                  .Select(
+                                     temp1 =>
+                                        new
+                                        {
+                                            labInsItem = temp1.LII.labInsItem,
+                                            checkbox = temp1.temp0.LID.checkbox,
+                                            comments = temp1.temp0.LID.comments
+                                        }
+                                  );
+        gvwLabFollowup.DataBind();
+    }
+
+    protected void gvwLabFollowup_RowDataBound(object sender, System.Web.UI.WebControls.GridViewRowEventArgs e)
+    {
+        if (e.Row.RowType == DataControlRowType.DataRow)
+        {
+            for (int i = -1; i < gvwLabFollowup.Rows.Count; i++)
+            {
+
+                if (e.Row.Cells[1].Text == "1")
+                    e.Row.Cells[1].Text = "Yes";
+
+                if (e.Row.Cells[1].Text == "2")
+                    e.Row.Cells[1].Text = "No";
+
+                if (e.Row.Cells[1].Text == "3")
+                    e.Row.Cells[1].Text = "N/A";
+
+            }
+        }
+    }
+    #endregion
+
+    #region Office Followup
+
+    #endregion
+}
