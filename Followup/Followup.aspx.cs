ac47556ca6721549f98abe9ebd4e8c127e7515ce
Chris Wood <chriswood.ca@gmail.com>
2011-05-17 15:50:31 -0700

    Office Inspection follow ups can now be completed.

--- Followup/Followup.aspx.cs
+++ Followup/Followup.aspx.cs
@@ -40,6 +40,7 @@ public partial class Followup_Followup : System.Web.UI.Page
                         break;
                     case "Office":
                         divLabOfficeFollowupReport.Visible = true;
+                        Populate_Office_Followup(Convert.ToInt32(tbNoHidden.Text));
                         break;
                     default:
                         throw new System.SystemException("Default case of switch should never be reached");
@@ -136,6 +137,52 @@ public partial class Followup_Followup : System.Web.UI.Page
     /// <summary>
     /// 
     /// </summary>
+    /// <param name="insNo"></param>
+    protected void Populate_Office_Followup(int insNo)
+    {
+        gvwLabOfficeFollowup.DataSource = ctx.OfficeInspections
+                                  .Join(
+                                     ctx.OfficeInspectionDetails,
+                                     OI => OI.officeInsNo,
+                                     OID => OID.officeInsNo,
+                                     (OI, OID) =>
+                                        new
+                                        {
+                                            OI = OI,
+                                            OID = OID
+                                        }
+                                  )
+                                  .Join(
+                                     ctx.OfficeInspectionItems,
+                                     temp0 => temp0.OID.officeInsItemNo,
+                                     OII => (Int32?)(OII.officeInsItemNo),
+                                     (temp0, OII) =>
+                                        new
+                                        {
+                                            temp0 = temp0,
+                                            OII = OII
+                                        }
+                                  )
+                                  .Where(temp1 => (temp1.temp0.OI.officeInsNo == insNo))
+                                  .Select(
+                                     temp1 =>
+                                        new
+                                        {
+                                            insItem = temp1.OII.officeInsName,
+                                            checkbox = temp1.temp0.OID.checkbox,
+                                            comments = temp1.temp0.OID.comments
+                                        }
+                                  );
+        gvwLabOfficeFollowup.DataBind();
+        gvwLabOfficeFollowup.Columns[0].ItemStyle.Width = 220;
+        gvwLabOfficeFollowup.Columns[1].ItemStyle.Width = 80;
+        gvwLabOfficeFollowup.Columns[2].ItemStyle.Width = 250;
+        gvwLabOfficeFollowup.Columns[3].ItemStyle.Width = 250;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
     /// <param name="sender"></param>
     /// <param name="e"></param>
     protected void tbSubmittedby_Load(object sender, EventArgs e)
@@ -180,7 +227,7 @@ public partial class Followup_Followup : System.Web.UI.Page
                 Submit_Lab_Followup();
                 break;
             case "Office":
-
+                Submit_Office_Followup();
                 break;
             default:
                 throw new System.SystemException("Default case of switch should never be reached");
@@ -235,9 +282,42 @@ public partial class Followup_Followup : System.Web.UI.Page
     /// </summary>
     protected void Submit_Office_Followup()
     {
+        int insNo = Convert.ToInt32(tbNoHidden.Text);
+        foreach (GridViewRow r in gvwLabOfficeFollowup.Rows)
+        {
+            //Decodes &codes;
+            string insItem = Server.HtmlDecode(r.Cells[0].Text);
 
+            int insItemNo = ctx.OfficeInspectionItems
+                    .Where(oii => oii.officeInsName == insItem)
+                    .Select(oii => oii.officeInsItemNo).First();
+            string insComment = ((TextBox)r.Cells[3].FindControl("tbCorrectiveAction")).Text;
+            ctx.AddToOfficeFollowUps(new OfficeFollowUp()
+            {
+                officeInsNo = insNo,
+                officeInsItemNo = insItemNo,
+                comment = insComment
+            });
+        }
+
+        OfficeInspection offIns = ctx.OfficeInspections
+            .Where(oi => oi.officeInsNo == insNo)
+            .Select(oi => oi).First();
+        offIns.followupComment = tbLabOfficeFollowupComments.Text;
+        offIns.followupSubmitter = tbSubmittedby.Text;
+        offIns.followupDate = Convert.ToDateTime(tbFollowUpDate.Text);
+        offIns.followUpStatus = "2";
+
+        try
+        {
+            ctx.SaveChanges();
+        }
+        catch (Exception ex)
+        {
+            Popup_Overlay(ex.Message, Color.Red);
+            return;
+        }
+        Popup_Overlay("Follow up Submitted.", Color.Green);
     }
     #endregion
-
-
 }
